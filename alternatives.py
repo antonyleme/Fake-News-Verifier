1b4b0584d0be52fcce052234847267496111e810
antonyleme <antonycefet@gmail.com>
2019-03-31 14:31:16 -0300

    Atualizacao no alternatives, main e news

--- alternatives.py
+++ alternatives.py
@@ -2,29 +2,14 @@ from googlesearch import search
 import news
 import difflib
 
+"""Método para pesquisar as fontes no google"""
 def find(term):
     results = []
     for url in search(term, stop=10):
         results.append(url)
     return results
 
-def get(term):
-    results = find(term)
-    alt = []
-    for result in results:
-        print(result)
-        try:
-            print(news.title(result))
-            sequence = difflib.SequenceMatcher(isjunk=None, a=term, b=news.title(result))
-            difference = sequence.ratio()*100
-            difference = round(difference,1)
-            print(difference)
-            if difference > 60:
-                alt.append(result)
-        except:
-            print("")
-    return media(alt)
-
+"""Método para pegar as médias dos ranks e das notas"""
 def media(alt):
     rank = 0
     note = 0
@@ -34,6 +19,25 @@ def media(alt):
         rank += inf[1] / alt.__len__()
     return [note, rank]
     
+"""Método para pegar todas as informações das fontes alternativas"""
+def get(term, url):
+    results = find(term)
+    alt = []
+    for result in results:
+        #print(result)
+        try:
+            #print(news.title(result))
+            sequence = difflib.SequenceMatcher(isjunk=None, a=term, b=news.title(result))
+            difference = sequence.ratio()*100
+            difference = round(difference,1)
+            #print(difference)
+            if difference > 60 and news.domain(result) != news.domain(url):
+                print(news.title(result))
+                alt.append(result)
+        except:
+            print("")
+
+    return [alt.__len__(), media(alt)]
     
         
 
--- news.py
+++ news.py
@@ -3,23 +3,31 @@ import urllib.request
 import requests
 from urllib.parse import urlparse
 
+"""Método para pegar o titulo de uma noticia a partir da url"""
 def title(url):
     soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
     title = soup.title.string.replace("|", "-").split("-")
     return title[0]
 
+"""Método para pegar o autor de uma notícia a partir da url"""
 def author(url):
     return 0
 
-def opr(domain):
+"""Método para pegar a nota e o rank de uma notícia a partir do domínio"""
+def opr(d):
     headers = {'API-OPR':'w0gswkcgcwcgs04o4kkcko0oo04k480co0gwg00k'}
-    url = 'https://openpagerank.com/api/v1.0/getPageRank?domains%5B0%5D=' + domain
+    url = 'https://openpagerank.com/api/v1.0/getPageRank?domains%5B0%5D=' + d
     request = requests.get(url, headers=headers)
     return request.json()
 
+"""Método para pegar o domínio"""
+def domain(url):
+    return urlparse(url).netloc
+
+"""Método para pegar todas as informações de uma notícia"""
 def get(url, primary):
-    domain = urlparse(url).netloc
-    apiRequest = opr(domain)
+    d = domain(url)
+    apiRequest = opr(d)
     note = float(apiRequest['response'][0]['page_rank_decimal']) / 10
     rank = 1 - float(apiRequest['response'][0]['rank']) / 3000000000
     if primary:
